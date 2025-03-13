1129f4b7f4e9aab8d38ca4c3faeaabaa033ca976
Techwolf78 <mrajaypawar207@gmail.com>
2025-03-13 12:24:51 +0530

    hi

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,9 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/', // Ensure base path is set to '/' for correct routing
+  build: {
+    rollupOptions: {
+      external: ['react-helmet'] // Mark react-helmet as external
+    }
+  }
 })
