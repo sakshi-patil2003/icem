0c0dd90eaddec3c14385a35297a891e9923f2ae1
sakshi-patil2003 <patilsakshi386@gmail.com>
2025-04-01 18:07:21 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import './App.css';
 import './index.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
-import Computer from './pages/Computer';
-import Mechanical from './pages/Mechanical';
+// import Computer from './pages/Computer';
+// import Mechanical from './pages/Mechanical';
 import Aids from './pages/Aids';  // Aids page with chatbot
-import IT from './pages/It';  // IT page with chatbot
+// import IT from './pages/It';  // IT page with chatbot
 import Entc from './pages/Entc';
 import Navbar from './pages/Navbar';
 import Footer from './pages/Footer';
@@ -18,10 +18,10 @@ function App() {
       <div className="relative">
         <Navbar />
         <Routes>
-          <Route path="/comp" element={<PageWithDynamicTitle title="Comp" />} />
-          <Route path="/mech" element={<PageWithDynamicTitle title="Mech" />} />
+          {/* <Route path="/comp" element={<PageWithDynamicTitle title="Comp" />} /> */}
+          {/* <Route path="/mech" element={<PageWithDynamicTitle title="Mech" />} /> */}
           <Route path="/aids" element={<PageWithDynamicTitle title="AIDS" />} />
-          <Route path="/it" element={<PageWithDynamicTitle title="IT" />} />
+          {/* <Route path="/it" element={<PageWithDynamicTitle title="IT" />} /> */}
           <Route path="/entc" element={<PageWithDynamicTitle title="ENTC" />} />
         </Routes>
         <Footer />
@@ -42,10 +42,10 @@ function PageWithDynamicTitle({ title }) {
       </Helmet>
 
       {/* Render the component based on the route */}
-      {title === "Comp" && <Computer />}
-      {title === "Mech" && <Mechanical />}
+      {/* {title === "Comp" && <Computer />} */}
+      {/* {title === "Mech" && <Mechanical />} */}
       {title === "AIDS" && <Aids />} {/* Aids page with chatbot */}
-      {title === "IT" && <IT />} {/* IT page with chatbot */}
+      {/* {title === "IT" && <IT />} IT page with chatbot */}
       {title === "ENTC" && <Entc />}
     </>
   );
